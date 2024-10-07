0aa62e7d1e94bbcccf1e8a24d7c5209bfe6c27d0
Ryan Lavery <ryan42fbd@gmail.com>
2024-10-07 17:08:26 -0500

    Set up firebase.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,54 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  InjectionToken,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp } from 'firebase/app';
+import {
+  Firestore,
+  initializeFirestore,
+  connectFirestoreEmulator,
+  getFirestore,
+} from 'firebase/firestore';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { environment } from '../environments/environment';
+
+const app = initializeApp(environment.firebase);
+
+export const AUTH = new InjectionToken('Firebase auth', {
+  providedIn: 'root',
+  factory: () => {
+    const auth = getAuth();
+    if (environment.useEmulators) {
+      connectAuthEmulator(auth, 'http://localhost:9099', {
+        disableWarnings: true,
+      });
+    }
+    return auth;
+  },
+});
+
+export const FIRESTORE = new InjectionToken('Firebase firestore', {
+  providedIn: 'root',
+  factory: () => {
+    let firestore: Firestore;
+    if (environment.useEmulators) {
+      firestore = initializeFirestore(app, {});
+      connectFirestoreEmulator(firestore, 'localhost', 8080);
+    } else {
+      firestore = getFirestore();
+    }
+    return firestore;
+  },
+});
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+  ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'ng-chat-997f7',
+    appId: '1:345387043877:web:dcc4d4e370aa298f80c3cc',
+    storageBucket: 'ng-chat-997f7.appspot.com',
+    apiKey: 'AIzaSyCp39lCoWg8mn-rSI4LFeIoJ_0QJUEFuxQ',
+    authDomain: 'ng-chat-997f7.firebaseapp.com',
+    messagingSenderId: '345387043877',
+  },
+  production: true,
+  useEmulators: false,
+};
